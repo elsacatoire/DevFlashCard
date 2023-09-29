6ff9082501a88f8e57a4264170acd1db3c130064
TheUtopy <109255677+TheUtopy@users.noreply.github.com>
2023-09-29 15:01:00 +0200

    feat : generation of the header component remove a useless .css file (we are using tailwind) Defined the locale data format to fr bg color is defined in the style.css file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'LOCALE_ID',
+    useValue: 'fr-FR'
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
